a089d4ee16dd75333e53c911172afa4d84974b16
uchikuch <ucheazinge@gmail.com>
2021-12-20 20:23:07 +0100

    Dockerfile

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json({ limit: "50mb" }));
 
 // Environment Variables
-const port = process.env.PORT;
+const port = process.env.PORT || 8080;
 const dbURI = process.env.mongoURI;
 
 // Database Connection
